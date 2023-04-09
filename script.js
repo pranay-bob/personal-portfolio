71cad71624e00dac50f0d2074db661be2573f0cf
Pranay Bobde <109841224+pranay-bob@users.noreply.github.com>
2023-04-09 11:08:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const videos = document.querySelectorAll(".video");
+let themeColor = document.querySelectorAll(".theme-toggler span");
+const menuOpen = document.querySelector(".menu-open");
+const menuClose = document.querySelector(".menu-close");
+const header = document.querySelector(".header");
+
+themeColor.forEach((color) =>
+  color.addEventListener("click", () => {
+    let background = color.style.background;
+    document.querySelector("body").style.background = background;
+    header.style.background = background;
+    menuOpen.style.background = background;
+    menuClose.style.background = background;
+  })
+);
+
+videos.forEach((video) => {
+  video.addEventListener("mouseover", () => {
+    video.play();
+    video.playbackRate = 2.0;
+  });
+  video.addEventListener("mouseout", () => {
+    video.pause();
+  });
+});
+
+// var type = new Typed(".typing-text", {
+//   strings: ["web designer", "front end developer", "Artist", "Guider"],
+//   typeSpeed: 120,
+//   loop: true,
+// });
+
+menuOpen.addEventListener("click", () => {
+  console.log("clicked");
+  header.classList.add("display");
+});
+
+menuClose.addEventListener("click", () => {
+  header.classList.remove("display");
+});
